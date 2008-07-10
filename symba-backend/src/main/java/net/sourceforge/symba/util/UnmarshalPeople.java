7868a0fb2cbbd6a1ab0401efd9990ed4119febda
allysonlister <allysonlister@576151d5-1e38-0410-a285-a06d94c17fb1>
2008-07-10 15:11:11 +0000

    removed extraneous try-catch blocks, and reformatted code.

--- symba-backend/src/main/java/net/sourceforge/symba/util/UnmarshalPeople.java
+++ symba-backend/src/main/java/net/sourceforge/symba/util/UnmarshalPeople.java
@@ -2,11 +2,6 @@ package net.sourceforge.symba.util;
 
 import net.sourceforge.symba.util.conversion.xml.PeopleUnmarshaler;
 
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.UnmarshalException;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 /*
  * This file is part of SyMBA.
  * SyMBA is covered under the GNU Lesser General Public License (LGPL).
@@ -24,36 +19,12 @@ import java.net.URISyntaxException;
 public class UnmarshalPeople {
 
     public static void main( String[] args ) throws Exception {
-        try {
 
-            if ( args.length != 1 )
-                throw new java.lang.Exception( "You must provide 1 argument: input-xml-file" );
+        if ( args.length != 1 )
+            throw new java.lang.Exception( "You must provide 1 argument: input-xml-file" );
 
-            System.out.println( "args[0]: " + args[0] );
-            PeopleUnmarshaler peopleUnmarshaler = new PeopleUnmarshaler( args[0] );
-            peopleUnmarshaler.Jaxb2ToFuGE();
-        } catch ( UnmarshalException ue ) {
-            // The JAXB specification does not mandate how the JAXB provider
-            // must behave when attempting to unmarshal invalid XML data.  In
-            // those cases, the JAXB provider is allowed to terminate the
-            // call to unmarshal with an UnmarshalException.
-            System.err.println( "Caught UnmarshalException:" );
-            ue.printStackTrace();
-        } catch ( JAXBException je ) {
-            System.err.println( "Caught JAXBException:" );
-            je.printStackTrace();
-        } catch ( IOException ioe ) {
-            System.err.println( "Caught IOException:" );
-            ioe.printStackTrace();
-        } catch ( URISyntaxException e ) {
-            System.err.println( "Caught URIException:" );
-            e.printStackTrace();
-        } catch ( fugeOM.service.RealizableEntityServiceException e ) {
-            System.err.println( "Caught RealizableEntityServiceException:" );
-            e.printStackTrace();
-        } catch ( Exception e ) {
-            System.err.println( "Caught General Exception:" );
-            e.printStackTrace();
-        }
+        System.out.println( "args[0]: " + args[0] );
+        PeopleUnmarshaler peopleUnmarshaler = new PeopleUnmarshaler( args[0] );
+        peopleUnmarshaler.Jaxb2ToFuGE();
     }
 }
--- symba-backend/src/main/java/net/sourceforge/symba/util/UnmarshalWorkflow.java
+++ symba-backend/src/main/java/net/sourceforge/symba/util/UnmarshalWorkflow.java
@@ -2,11 +2,6 @@ package net.sourceforge.symba.util;
 
 import net.sourceforge.symba.util.conversion.xml.WorkflowUnmarshaler;
 
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.UnmarshalException;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 /*
  * This file is part of SyMBA.
  * SyMBA is covered under the GNU Lesser General Public License (LGPL).
@@ -22,35 +17,14 @@ import java.net.URISyntaxException;
 public class UnmarshalWorkflow {
 
     public static void main( String[] args ) throws Exception {
-        try {
-            if ( args.length < 2 )
-                throw new java.lang.Exception(
-                        "You must provide at least 2 arguments: output-logfile input-xml-file [other input xmlfiles]" );
+        if ( args.length < 2 )
+            throw new java.lang.Exception(
+                    "You must provide at least 2 arguments: output-logfile input-xml-file [other input xmlfiles]" );
 
-            System.out.println( "args[0]: " + args[0] );
-            WorkflowUnmarshaler unmarshaler = new WorkflowUnmarshaler( args[0] );
-            unmarshaler.Jaxb2ToFuGE( args );
+        System.out.println( "args[0]: " + args[0] );
+        WorkflowUnmarshaler unmarshaler = new WorkflowUnmarshaler( args[0] );
+        unmarshaler.Jaxb2ToFuGE( args );
 
-        } catch ( UnmarshalException ue ) {
-            // The JAXB specification does not mandate how the JAXB provider
-            // must behave when attempting to unmarshal invalid XML data.  In
-            // those cases, the JAXB provider is allowed to terminate the
-            // call to unmarshal with an UnmarshalException.
-            System.err.println( "Caught UnmarshalException:" );
-            ue.printStackTrace();
-        } catch ( JAXBException je ) {
-            je.printStackTrace();
-        } catch ( IOException ioe ) {
-            ioe.printStackTrace();
-        } catch ( URISyntaxException e ) {
-            e.printStackTrace();
-        } catch ( fugeOM.service.RealizableEntityServiceException e ) {
-            System.err.println( "Caught RealizableEntityServiceException:" );
-            e.printStackTrace();
-        } catch ( Exception e ) {
-            System.err.println( "Caught General Exception:" );
-            e.printStackTrace();
-        }
 
     }
 }

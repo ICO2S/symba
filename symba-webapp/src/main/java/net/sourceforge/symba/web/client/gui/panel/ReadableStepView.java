81e2f50437e80c1985bbee3b04d056efea011e84
allysonlister <allysonlister@576151d5-1e38-0410-a285-a06d94c17fb1>
2010-03-17 10:10:08 +0000

    made collapsable fields for files and parameters

--- symba-webapp/src/main/java/net/sourceforge/symba/web/client/gui/panel/ReadableStepView.java
+++ symba-webapp/src/main/java/net/sourceforge/symba/web/client/gui/panel/ReadableStepView.java
@@ -1,7 +1,9 @@
 package net.sourceforge.symba.web.client.gui.panel;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import net.sourceforge.symba.web.client.stepsorter.ExperimentParameter;
@@ -24,14 +26,7 @@ public class ReadableStepView extends VerticalPanel {
 
         this.stepTitle = stepTitle;
         this.fileNames = fileNames;
-
-        Label label = new Label( this.stepTitle );
-        add( label );
-
-        Label label2 = new Label( displayFileNames() );
-        add( label2 );
-        add( parameterTable );
-
+        setupView();
     }
 
     public ReadableStepView(
@@ -44,14 +39,47 @@ public class ReadableStepView extends VerticalPanel {
 
         this.stepTitle = stepTitle;
         this.fileNames = fileNames;
+        setupView( myEditableHandler );
+    }
+
+    private void setupView() {
+        setupView( null );
+    }
+
+    private void setupView( ClickHandler myEditableHandler ) {
 
         Label label = new Label( this.stepTitle );
-        label.addClickHandler( myEditableHandler );
         add( label );
-        label.addStyleName( "clickable-text" );
 
-        Label label2 = new Label( displayFileNames() );
-        add( label2 );
+        HorizontalPanel hPanel = new HorizontalPanel();
+        hPanel.setSpacing( 5 );
+        final Label fileLabel = new Label( displayFileNameCount() );
+        final Label actionLabel = new Label( "(expand)" );
+        actionLabel.addStyleName( "clickable-text" );
+        actionLabel.addClickHandler( new ClickHandler() {
+            public void onClick( ClickEvent event ) {
+                if ( actionLabel.getText().equals( "(expand)" ) ) {
+                    actionLabel.setText( "(collapse)" );
+                    fileLabel.setText( displayFileNames() );
+                } else {
+                    actionLabel.setText( "(expand)" );
+                    fileLabel.setText( displayFileNameCount() );
+                }
+            }
+        } );
+        if ( myEditableHandler != null ) {
+            label.addClickHandler( myEditableHandler );
+            label.addStyleName( "clickable-text" );
+            fileLabel.addClickHandler( myEditableHandler );
+            fileLabel.addStyleName( "clickable-text" );
+        }
+
+        if ( fileNames.size() > 0 ) {
+            hPanel.add( fileLabel );
+            hPanel.add( actionLabel );
+            add( hPanel );
+        }
+
         add( parameterTable );
     }
 
@@ -75,6 +103,17 @@ public class ReadableStepView extends VerticalPanel {
         return text;
     }
 
+    private String displayFileNameCount() {
+        if ( fileNames.size() == 1 ) {
+            return "1 data file ";
+        } else if ( fileNames.size() > 1 ) {
+            return fileNames.size() + " data files ";
+        }
+
+        return "";
+    }
+
+
     public class ReadableStepParameterTable extends FlexTable {
         // each row is plain text
         private ArrayList<ExperimentParameter> parameters;
@@ -85,9 +124,48 @@ public class ReadableStepView extends VerticalPanel {
         }
 
         private ReadableStepParameterTable( ArrayList<ExperimentParameter> parameters,
-                                            ClickHandler myEditableHandler ) {
-            rowCount = 0;
+                                            final ClickHandler myEditableHandler ) {
+            rowCount = 1; // start at 1, as 0 is reserved for the summary of the parameters
             this.parameters = parameters;
+
+            HorizontalPanel hPanel = new HorizontalPanel();
+            hPanel.setSpacing( 5 );
+            final Label parameterSummaryLabel = new Label( displayParameterCount() );
+            final Label actionLabel = new Label( "(expand)" );
+            actionLabel.addStyleName( "clickable-text" );
+            actionLabel.addClickHandler( new ClickHandler() {
+                public void onClick( ClickEvent event ) {
+                    if ( actionLabel.getText().equals( "(expand)" ) ) {
+                        actionLabel.setText( "(collapse)" );
+                        displayParameters( myEditableHandler );
+                    } else {
+                        actionLabel.setText( "(expand)" );
+                        hideParameters();
+                    }
+                }
+            } );
+            if ( myEditableHandler != null ) {
+                parameterSummaryLabel.addClickHandler( myEditableHandler );
+                parameterSummaryLabel.addStyleName( "clickable-text" );
+            }
+
+            if ( parameters.size() > 0 ) {
+                hPanel.add( parameterSummaryLabel );
+                hPanel.add( actionLabel );
+                setWidget( 0, 0, hPanel );
+            }
+
+        }
+
+        private void hideParameters() {
+            if ( parameters.size() > 0 ) {
+                for ( int iii = getRowCount(); iii > 1; iii-- ) {
+                    remove( getWidget( iii - 1, 0 ) );
+                }
+            }
+        }
+
+        private void displayParameters( ClickHandler myEditableHandler ) {
 
             for ( ExperimentParameter parameter : this.parameters ) {
                 Label label = new Label(
@@ -99,6 +177,15 @@ public class ReadableStepView extends VerticalPanel {
                     label.addStyleName( "clickable-text" );
                 }
             }
+        }
+
+        private String displayParameterCount() {
+            if ( parameters.size() == 1 ) {
+                return "1 parameter ";
+            } else if ( parameters.size() > 1 ) {
+                return parameters.size() + " parameters";
+            }
+            return "";
         }
 
         public ArrayList<ExperimentParameter> getParameters() {
